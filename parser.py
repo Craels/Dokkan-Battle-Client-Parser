1a8dc0550fd27fb9a556090c7ecdd670203f5579
Craels <68694284+Craels@users.noreply.github.com>
2020-08-15 23:00:41 +1000

    Changed the APK host provider for a more reliable source

--- parser.py
+++ parser.py
@@ -1,50 +1,67 @@
 import requests
-import shutil
 import os
+import shutil
 import re
+import sys
+import json
 from zipfile import ZipFile
 
-class Parser:
-    def __init__(self):
+class Update:
+    def __init__(self, version, server):
         self.s = requests.session()
-        
-    def downloadAPK(self, version):
-        print('[+] Finding the current available version')
-        url = 'https://apkpure.com/dragon-ball-z-dokkan-battle/com.bandainamcogames.dbzdokkanww'
-        resp = self.s.get(url)
-        apkPureVersion = self.getBetween(resp.text, 'version">', ' </span>')
-        print(f'[+] Found version {apkPureVersion}. Checking if its a new version')
-        if version < apkPureVersion:
-            print(f'[+] New version detected, downloading Dokkan Battle Version {apkPureVersion}')
-            url = 'https://apkpure.com/dragon-ball-z-dokkan-battle/com.bandainamcogames.dbzdokkanww/download?from=details'
-            resp = self.s.get(url)
-            apkLink = self.getBetween(resp.text, 'https://download.apkpure.com/', '">click')
-            resp = self.s.get(f'https://download.apkpure.com/{apkLink}', stream=True)
-            fileSize = int(resp.headers['Content-Length'])
-            dl = 0
-            with open('DokkanBattle.apk', 'wb') as f:
-                for chunk in resp.iter_content(chunk_size = 1024*1024):
-                    if chunk:
-                        dl += len(chunk)
-                        print(f'[+] {int(100 * dl / fileSize)}% Downloaded..', end='\r')
-                        f.write(chunk)
-            print('[+] Finished downloading. Extracting the new version code..')
-        else:
-            print(f'[-] Version {apkPureVersion} is not a new version, try again later.')
+        self.version = version
+        self.server = server
 
-    def verionCode(self):
+    def downloadAPK(self):
+        if self.server == 'ishin-global.aktsk.com':
+            _Package = 'https://api.qoo-app.com/v6/apps/com.bandainamcogames.dbzdokkanww/download?version_code='
+            _Page = 'https://apps.qoo-app.com/en/app/2519'
+        else:
+            _Package = 'https://api.qoo-app.com/v6/apps/com.bandainamcogames.dbzdokkan/download?version_code='
+            _Page = 'https://apps.qoo-app.com/en/app/191'
+        resp = self.s.get(_Page)
+        _QooVersion = self.getBetween(resp.text, 'softwareVersion":"', '","fileSize"')
+        if _QooVersion != self.version:
+            resp = self.s.get('https://api.qoo-app.com/v6/apps')
+            try:
+                _VersionCode = resp.json()['apps'][0]['version_code']
+            except KeyError:
+                _VersionCode = 999
+            _Package = _Package + str(_VersionCode)
+            resp = self.s.get(_Package, stream = True)
+            _FileSize = int(resp.headers['Content-Length'])
+            _Download = 0
+            if _FileSize < 80000000:
+                print('[-] Incorrect file size, Qoo probably changed the download properties. Press enter to exit!')
+                input()
+                sys.exit()
+            with open('DokkanBattle.apk', 'wb') as f:
+                for chunk in resp.iter_content(chunk_size=1024*1024):
+                    if chunk:
+                        _Download += len(chunk)
+                        print(f'[+] {int(100 * _Download / _FileSize)}% Downloaded..', end='\r')
+                        f.write(chunk)
+            print('[+] Finished downloading.')
+            _ClientVersion = self.extractClient(_QooVersion)
+            return _ClientVersion
+        else:
+            print('[-] A new version doesn\'t exist..')
+
+    def extractClient(self, _version):
         os.rename('DokkanBattle.apk', 'DokkanBattle.zip')
-        os.mkdir('data')
-        ZipFile('DokkanBattle.zip').extractall('data')
-        with open('data/lib/arm64-v8a/libcocos2dcpp.so', 'r', encoding='cp437') as f:
-            versionCode = re.findall(r'[0-9a-f]{64}', f.read())
-        return versionCode[0]
+        os.mkdir('APK')
+        ZipFile('DokkanBattle.zip').extractall('APK')
+        with open('APK/lib/armeabi-v7a/libcocos2dcpp.so', 'r', encoding='cp437') as f:
+            _VersionCode = re.findall(r'[0-9a-f]{64}', f.read())
+            for data in _VersionCode:
+                if not data.isdigit():
+                    return f'{_version}-{data}'
 
     def cleanUp(self):
-        if os.path.exists('data'):
-            shutil.rmtree('data')
+        if os.path.exists('APK'):
+            shutil.rmtree('APK')
         if os.path.exists('DokkanBattle.zip'):
             os.remove('DokkanBattle.zip')
- 
+
     def getBetween(self, data, first, last):
         return data.split(first)[1].split(last)[0]
